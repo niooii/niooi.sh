e25d0bae8109fe05543940ce467b6d9978b368a6
onioniwonpounoin <wangh3@bxscience.edu>
2024-12-12 13:40:29 -0500

    falling snow wow chat

--- frontend/src/components/seasonal_deco/winter.tsx
+++ frontend/src/components/seasonal_deco/winter.tsx
@@ -1,9 +1,5 @@
 "use client"
-import React, { useEffect, useRef, useState } from 'react';
-
-function image(): HTMLImageElement {
-
-}
+import React, { RefObject, useEffect, useRef, useState } from 'react';
 
 class SnowParticle 
 {
@@ -14,9 +10,9 @@ class SnowParticle
     rotation: number;
     rotationSpeed: number;
     opacity: number;
-    img: HTMLImageElement;
+    img: RefObject<HTMLImageElement | undefined>;
 
-    constructor(x: number, y: number, img: HTMLImageElement, options: Partial<SnowParticle> = {}) {
+    constructor(x: number, y: number, img: RefObject<HTMLImageElement | undefined>, options: Partial<SnowParticle> = {}) {
         this.x = x;
         this.y = y;
         this.speed = options.speed || 0.5 + Math.random() * 1;
@@ -28,11 +24,16 @@ class SnowParticle
     }
 
     public update(delta_time: number) {
-
+        this.y += delta_time * 10;
     }
 
     public draw(ctx: CanvasRenderingContext2D) {
-        ctx.drawImage(this.img, this.x, this.y)
+        if (this.img.current == undefined)
+            return;
+        ctx.save();
+        ctx.rotate(Math.PI/45);
+        ctx.drawImage(this.img.current, this.x, this.y)
+        ctx.restore();
     }
 }
 
@@ -45,6 +46,10 @@ const Winter = ({  }: WinterProps) => {
     const animationFrameId = useRef<number | undefined>(undefined);
     const previousTime = useRef<number | undefined>(undefined);
     const yRef = useRef<number>(0);
+
+    for (let i = 0; i < 5; i++) {
+        particles.current.push(new SnowParticle(Math.random() * 30, Math.random() * 30, imageRef))
+    }
 
     useEffect(() => {
         const img = new Image();
@@ -76,14 +81,10 @@ const Winter = ({  }: WinterProps) => {
                 ctx.font = "30px Arial";
                 ctx.fillText("" + (Math.random() * 500).toFixed(0), 10, 50);
                 ctx.fillText("y: " + yRef.current, 10, 90);
-                if (imageRef.current === undefined)
-                {
-                    console.log("UNDEF");
-                }
-                else
-                {
-                    ctx.drawImage(imageRef.current, 20, yRef.current * 100)
-                }
+                particles.current.forEach(p => {
+                    p.draw(ctx);
+                    p.update(deltaTime);
+                });
 
                 console.log("hi chat");
             }
@@ -100,7 +101,7 @@ const Winter = ({  }: WinterProps) => {
         
         resizeCanvas();
         
-        animate();
+        animate(0);
         
         return () => {
             console.log('Cleaning up animation');
